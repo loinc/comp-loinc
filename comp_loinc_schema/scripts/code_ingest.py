224a7180ef1b8599017e187647e55776b3fd98c4
Tim  Putman <putmantime@users.noreply.github.com>
2022-04-05 12:29:52 -0700

    ingesting loinc codes with their system and component parts

--- comp_loinc_schema/scripts/code_ingest.py
+++ comp_loinc_schema/scripts/code_ingest.py
@@ -11,27 +11,23 @@ import json
 lpl = pd.read_excel('../data/CHEM_HIERARCHY_LPL_DATA.xlsx', sheet_name="LPL")
 sv = SchemaView('../model/schema/code_schema.yaml')
 od = OWLDumper()
-
 code_classes = []
 for index, code in enumerate(lpl.itertuples()):
-    if index < 10:
-        if code.NAME == "COMPONENT":
-            code_class = LoincCodeClass(
-                id=loincify(code.LOINC_NUM),
-                label=code.LONG_COMMON_NAME,
-                has_component=loincify(code.PART_NUM))
-            code_classes.append(code_class)
-        if code.NAME == "SYSTEM":
-            code_class = LoincCodeClass(
-                id=loincify(code.LOINC_NUM),
-                label=code.LONG_COMMON_NAME,
-                has_system=loincify(code.PART_NUM))
-            code_classes.append(code_class)
+    if code.NAME == "COMPONENT":
+        code_class = LoincCodeClass(
+            id=loincify(code.LOINC_NUM),
+            label=code.LONG_COMMON_NAME,
+            has_component=loincify(code.PART_NUM))
+        code_classes.append(code_class)
+    if code.NAME == "SYSTEM":
+        code_class = LoincCodeClass(
+            id=loincify(code.LOINC_NUM),
+            label=code.LONG_COMMON_NAME,
+            has_system=loincify(code.PART_NUM))
+        code_classes.append(code_class)
 with open("../data/output/code_classes.owl", 'w') as ccl_owl:
     ccl_owl.write(od.dumps(code_classes, schema=sv.schema))
-loinc_ontology_class = LoincCodeOntology(code_class_set=code_classes)
+# loinc_ontology_class = LoincCodeOntology(code_class_set=code_classes)
 
-with open("../data/output/code_classes.json", 'w') as ccl_json:
-    ccl_json.write(json_dumper.dumps(loinc_ontology_class))
-    # joined = ',\n'.join([json_dumper.dumps(x) for x in code_classes])
-    # ccl_json.write(f"[\n{joined}\n]")
+# with open("../data/output/code_classes.json", 'w') as ccl_json:
+#     ccl_json.write(json_dumper.dumps(loinc_ontology_class))
