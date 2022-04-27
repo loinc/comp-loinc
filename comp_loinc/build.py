35fcdabceffb0296969f4551177c0397924e15e8
Tim  Putman <putmantime@users.noreply.github.com>
2022-04-27 14:02:38 -0700

    added readme and full workflow

--- comp_loinc/build.py
+++ comp_loinc/build.py
@@ -10,11 +10,13 @@ app = typer.Typer()
 @app.command(name='parts')
 def build_part_ontology(
         schema_file: str,
-        part_file: str,
+        part_primary_file: str,
+        part_supplementary_file: str,
         hierarchy_file: str,
         output: str):
     po = PartOntology(schema_path=schema_file)
-    po.generate_part_type_lookup(loinc_part_file=part_file)
+    po.generate_part_type_lookup(loinc_primary_part_file=part_primary_file,
+                                 loinc_supplement_part_file=part_supplementary_file)
     po.generate_part_hierarchy_lookup(chem_hierarchy_file=hierarchy_file)
     po.generate_ontology()
     po.write_to_output(output_path=output)
@@ -28,6 +30,20 @@ def build_codes(
     cp = CodeIngest(schema_path=schema_file, code_file_path=code_file)
     cp.generate_codes()
     cp.write_output_to_file(output_path=output)
+
+@app.command(name='composed')
+def build_composed_classes(
+        schema_file: str,
+        composed_classes_data_file: str,
+        output: str):
+    """
+    TODO this is just calling a linkml owl cli, should be written as code
+    :param schema_file:
+    :param code_file:
+    :param output:
+    :return:
+    """
+    subprocess.call(["linkml-data2owl", "-s", schema_file, composed_classes_data_file, "-o", output])
 
 @app.command(name="merge")
 def merge_owl(
