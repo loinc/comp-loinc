b2c3517207ac6b391a179433150367a526443497
Joe Flack <joeflack4@gmail.com>
2024-12-22 17:23:16 -0500

    NLP on dangling parts

--- src/comp_loinc/groups/groups_builders.py
+++ src/comp_loinc/groups/groups_builders.py
@@ -6,7 +6,6 @@ import typing as t
 from pathlib import Path
 from pickle import Pickler
 
-import pandas as pd
 import typer
 
 import comp_loinc as cl
@@ -18,6 +17,7 @@ from loinclib.loinc_schema import LoincNodeType, LoincTermPrimaryEdges, LoincPar
   LoincTermSupplementaryEdges
 from loinclib.loinc_tree_loader import LoincTreeLoader
 from loinclib.loinc_tree_schema import LoincTreeEdges, LoincTreeProps
+from loinclib.nlp_taxonomification import parts_to_tsv
 
 
 class Index:
@@ -160,20 +160,6 @@ class GroupsBuilderSteps:
     builder.cli.command('groups-index-props', help='Indexes the use of LOINC properties by property and part.')(
         self.__main)
 
-  def parts_to_tsv(self, parts: t.List[Part]):
-    """Save list of Part objects to TSV."""
-    outdir = self.config.home_path / 'output' / 'analysis' / 'dangling'
-    outpath = outdir / 'dangling.tsv'
-    if not outdir.exists():
-      outdir.mkdir(parents=True)
-    df = pd.DataFrame([{
-        'id': p.part_number,
-        'display': p.part_display,
-        'type': p.part_type,
-        'is_search': p.is_search,
-    } for p in parts])
-    df.to_csv(outpath, sep='\t', index=False)
-
   def __main(self,
       supplementary: t.Annotated[bool, typer.Option('--supl', help='Use primary vs supplementary.')] = False,
       _pickle: t.Annotated[bool, typer.Option('--pickle', help='Use primary vs supplementary.')] = False
@@ -197,7 +183,9 @@ class GroupsBuilderSteps:
 
     # Save dangling parts
     dangling: t.Dict[str, Part] = self.index.parts_roots_no_children
-    self.parts_to_tsv(list(dangling.values()))
+    parts_to_tsv(
+      list(dangling.values()),
+      self.config.home_path / 'output' / 'analysis' / 'dangling' / 'dangling.tsv')
 
   def do_abstracts2(self):
 
