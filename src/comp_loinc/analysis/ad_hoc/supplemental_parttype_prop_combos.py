e151eba24680fa2ad43434f54efc74ec0d53fb93
Joe Flack <joeflack4@gmail.com>
2025-06-19 15:24:44 -0400

    Check all of the different properties and their part types - Moved file. added comments. refactored slightly
    General
    - Renamed some other analysis modules

---
+++ src/comp_loinc/analysis/ad_hoc/supplemental_parttype_prop_combos.py
@@ -0,0 +1,46 @@
+"""Check all of the different properties and their part types
+
+Result:
+PartTypeName|Property
+ADJUSTMENT 		| adjustment
+CHALLENGE 		| search
+CHALLENGE 		| challenge
+CLASS 		| category
+CLASS 		| CLASS
+COMPONENT 		| analyte
+COMPONENT 		| analyte-core
+COMPONENT 		| search
+COUNT 		| count
+DIVISOR 		| analyte-divisor
+GENE 		| analyte-gene
+METHOD 		| search
+METHOD 		| METHOD_TYP
+NUMERATOR 		| analyte-numerator
+PROPERTY 		| PROPERTY
+SCALE 		| SCALE_TYP
+SUFFIX 		| analyte-suffix
+SUFFIX 		| analyte-divisor-suffix
+SUPER SYSTEM 		| super-system
+SUPER SYSTEM 		| search
+SYSTEM 		| system-core
+SYSTEM 		| search
+TIME 		| time-core
+TIME MODIFIER 		| time-modifier
+"""
+from pathlib import Path
+
+import pandas as pd
+
+PROJ_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
+PATH = PROJ_DIR / 'loinc_release/Loinc_2.80/AccessoryFiles/PartFile/LoincPartLink_Supplementary.csv'
+
+
+df = pd.read_csv(PATH)
+unique_combinations = df[['PartTypeName', 'Property']].drop_duplicates().sort_values(by='PartTypeName')
+outpath = '~/Desktop/suppl.tsv'
+# unique_combinations.to_csv(outpath, sep='\t', index=False)
+unique_combinations['Property'] = (
+    unique_combinations['Property'].str.replace('http://loinc.org/property/', '', regex=False))
+print('PartTypeName|Property')
+for _, row in unique_combinations.iterrows():
+    print(f"{row['PartTypeName']} \t\t| {row['Property']}")
